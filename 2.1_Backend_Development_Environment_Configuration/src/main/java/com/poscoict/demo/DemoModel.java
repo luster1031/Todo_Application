4aca6d31865668cb0c56a44af7922233a2f54d78
luster1031 <luster47@naver.com>
2022-03-07 23:49:13 +0900

    [configuration] Test example demo

---
+++ 2.1_Backend_Development_Environment_Configuration/src/main/java/com/poscoict/demo/DemoModel.java
@@ -0,0 +1,12 @@
+package com.poscoict.demo;
+
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Builder
+@RequiredArgsConstructor
+public class DemoModel {
+	@NonNull
+	private String id;
+}
---
+++ 2.1_Backend_Development_Environment_Configuration/src/test/java/com/poscoict/demo/DemoApplicationTest.java
@@ -0,0 +1,12 @@
+package com.poscoict.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = DemoApplication.class)
+public class DemoApplicationTest {
+	@Test
+	public void contextLoads() {
+		System.out.println("g");
+	}
+}
